490c5ec2382673d9eb92136b0450102c31ff2454
Farhat-Binte-Shahid <arpafarhat@gmail.com>
2022-09-20 18:22:54 +0600

    AllLeave by Farhat-Binte-Shahid

---
+++ src/components/AllLeave.js
@@ -0,0 +1,181 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+
+const AllLeave = (props) => {
+  return (
+    <>
+      <Helmet>
+        <title>All Leave | BUP Leave Register</title>
+      </Helmet>
+      <div id="main">
+        <nav className="navbar navbar-header navbar-expand navbar-light">
+          <div onClick={() => props.updateSidebarState()} className="sidebar-toggler" style={{ cursor: "pointer" }}>
+            <span className="navbar-toggler-icon"></span>
+          </div>
+          <button
+            className="btn navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav d-flex align-items-center navbar-light ms-auto">
+              <li className="dropdown nav-icon">
+                <a href="#" data-bs-toggle="dropdown" className="nav-link  dropdown-toggle nav-link-lg nav-link-user">
+                  <div className="d-lg-inline-block">
+                    <i data-feather="bell"></i>
+                    <span className="badge bg-info">2</span>
+                  </div>
+                </a>
+                <div className="dropdown-menu dropdown-menu-end dropdown-menu-large">
+                  <h6 className="py-2 px-4">Notifications</h6>
+                  <ul className="list-group rounded-none">
+                    <li className="list-group-item border-0 align-items-start">
+                      <div className="row mb-2">
+                        <div className="col-md-12 notif">
+                          <a href="leave_details.html">
+                            <h6 className="text-bold">John Doe</h6>
+                            <p className="text-xs">applied for leave at 05-21-2021</p>
+                          </a>
+                        </div>
+                        <div className="col-md-12 notif">
+                          <a href="leave_details.html">
+                            <h6 className="text-bold">Jane Doe</h6>
+                            <p className="text-xs">applied for leave at 05-21-2021</p>
+                          </a>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li className="dropdown">
+                <a href="#" data-bs-toggle="dropdown" className="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                  <div className="avatar me-1">
+                    <img src="/assets/images/admin.png" alt="" />
+                  </div>
+                  <div className="d-none d-md-block d-lg-inline-block">Hi, Admin</div>
+                </a>
+                <div className="dropdown-menu dropdown-menu-end">
+                  <a className="dropdown-item" href="#">
+                    <i data-feather="user"></i> Account
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    <i data-feather="settings"></i> Settings
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <Link to="/" className="dropdown-item">
+                    <i data-feather="log-out"></i> Logout
+                  </Link>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="main-content container-fluid">
+          <div className="page-title">
+            <div className="row">
+              <div className="col-12 col-md-6 order-md-1 order-last">
+                <h3>Leave Application</h3>
+              </div>
+              <div className="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" className="breadcrumb-header">
+                  <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <Link to="/admin" className="text-success">
+                        Dashboard
+                      </Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Leave Application
+                    </li>
+                  </ol>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <section className="section">
+            <div className="card">
+              <div className="card-body">
+                <table className="table" id="table1">
+                  <thead>
+                    <tr>
+                      <th>Full Name</th>
+                      <th>Leave Type</th>
+                      <th>Posting Date</th>
+                      <th>Status</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>John Doe</td>
+                      <td>Sick Leave</td>
+                      <td>2021-11-01</td>
+                      <td>
+                        <span className="badge bg-success">Approved</span>
+                      </td>
+                      <td>
+                        <a href="editDesignation.php">
+                          <i className="fa fa-eye text-success"></i>
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Juan Dela Cruz</td>
+                      <td>Sick Leave</td>
+                      <td>2021-11-01</td>
+                      <td>
+                        <span className="badge bg-info">Pending</span>
+                      </td>
+                      <td>
+                        <a href="editDesignation.php">
+                          <i className="fa fa-eye text-success"></i>
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Cardo Dalisay</td>
+                      <td>Medical Leave</td>
+                      <td>2021-11-01</td>
+                      <td>
+                        <span className="badge bg-danger">Not Approved</span>
+                      </td>
+                      <td>
+                        <a href="editDesignation.php">
+                          <i className="fa fa-eye text-success"></i>
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>John Doe</td>
+                      <td>Sick Leave</td>
+                      <td>2021-11-01</td>
+                      <td>
+                        <span className="badge bg-success">Approved</span>
+                      </td>
+                      <td>
+                        <a href="editDesignation.php">
+                          <i className="fa fa-eye text-success"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AllLeave;
